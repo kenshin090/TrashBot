fb8588ad48a078434afbed32413274fd21d287b8
ramiro.contreras <ramiro.contreras@ticxar.com>
2018-06-23 10:54:00 -0500

    avance

--- TrashBot/Code/TrashBot-web/src/app/app.module.ts
+++ TrashBot/Code/TrashBot-web/src/app/app.module.ts
@@ -1,11 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LogincomponentComponent } from './login/logincomponent/logincomponent.component';
+
+const appRoutes: Routes = [
+  {
+      path: '',
+      redirectTo: 'login', pathMatch: 'full'
+  },
+  {
+    path: '**', component: LogincomponentComponent
+  }
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogincomponentComponent
   ],
   imports: [
     BrowserModule
