646719f181168be841ee14363b277c5d5ed2bbca
Daniel Acosta <daniele-acostah@unilibre.edu.co>
2018-07-11 20:29:43 -0500

    backend

---
+++ TrashBot/Code/TrashBot-BackEnd/trashbot_api_app/app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  before_action :authenticate_request, except: %i[login register]
+   # POST /register
+    def register
+      @user = User.create(user_params)
+     if @user.save
+      response = { message: 'User created successfully'}
+      render json: response, status: :created 
+     else
+      render json: @user.errors, status: :bad
+     end 
+    end
+
+    def login
+      authenticate params[:email], params[:password]
+    end
+    
+    def test
+      render json: {
+            message: 'You have passed authentication and authorization test'
+          }
+    end
+  
+    private  
+    def user_params
+      params.permit(
+        :name,
+        :email,
+        :password
+      )
+    end
+
+    def authenticate(email, password)
+      command = AuthenticateUser.call(email, password)
+  
+      if command.success?
+        render json: {
+          access_token: command.result,
+          message: 'Login Successful'
+        }
+      else
+        render json: { error: command.errors }, status: :unauthorized
+      end
+     end  
+  end
